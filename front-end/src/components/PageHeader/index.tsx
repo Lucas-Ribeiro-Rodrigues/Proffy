3cf2849594fb517ed480510ac5c16bd417d2fad8
Lucas Ribeiro <lucas.rodrigues@dextra-sw.com>
2020-09-09 19:58:14 -0300

    Add pages and components

--- front-end/src/components/PageHeader/index.tsx
+++ front-end/src/components/PageHeader/index.tsx
@@ -7,7 +7,8 @@ import backIcon from '../../assets/images/icons/back.svg'
 import './styles.css'
 
 interface PageHeaderProps {
-  title: String;
+  title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -15,13 +16,15 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
     <header className="page-header">
       <div className="top-bar-container">
         <Link to="/">
-          <img src={backIcon} alt="Voltar"/>
+          <img src={backIcon} alt="Voltar" />
         </Link>
-        <img src={logoImg} alt="Proffy"/>
+        <img src={logoImg} alt="Proffy" />
       </div>
 
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.description && <p>{props.description}</p>}
+
         {props.children}
       </div>
     </header>
