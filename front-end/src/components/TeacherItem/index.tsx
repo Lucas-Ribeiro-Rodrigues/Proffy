bcd4a7d0bcd92c0c42df837d07f7352ac8b6d32d
Lucas Ribeiro <lucas.rodrigues@dextra-sw.com>
2020-09-10 13:40:40 -0300

    Integrating with back-end

--- front-end/src/components/TeacherItem/index.tsx
+++ front-end/src/components/TeacherItem/index.tsx
@@ -3,35 +3,56 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher; 
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
-          <header>
-            <img src="https://avatars1.githubusercontent.com/u/20904531?s=460&u=945e3ea4c401d77604ce1f60b6845d4655ed2b3e&v=4" alt="Lucas Ribeiro"/>
-            <div>
-              <strong>Lucas Ribeiro</strong>
-              <span>Matemática</span>
-            </div>
-          </header>
+      <header>
+        <img src={teacher.avatar} alt="Foto" />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
 
-          <p>
-            Entusiasta do symbolab e de polinômios
-            <br/><br/>
-            Gosta muito de férias
-          </p>
+      <p>{teacher.bio}</p>
 
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 100,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="Whatsapp" />
+      <footer>
+        <p>
+          Preço/hora
+          <strong>{teacher.cost}</strong>
+        </p>
+        <a
+          target="_blank" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
+          <img src={whatsappIcon} alt="Whatsapp" />
               Entrar em contato
-            </button>
-          </footer>
-        </article>
+        </a>
+      </footer>
+    </article>
   )
 }
 
